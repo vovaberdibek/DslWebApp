dddfb3eeadf10b8bcf0a53f9ee189c8c07a2acdd
Vladimir_Berdibekov <vladimir.berdibekov@gransassotech.org>
2025-07-01 12:47:41 +0200

    x exit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  // allow re-use of PrismaClient in dev
+  // (avoids hot-reload exhaustion of connections)
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'], // you can remove this if you don’t want SQL logs
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
+}
